85c0c3a9fab0edcde0048a2f1b477455366852c5
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2023-05-13 22:47:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def predict_image_file():
 
 # Driver code
 if __name__ == "__main__":
-    app.run(port=9000, debug=True)
+    app.run(port=9000, debug=True, host = "https://baby-blue-salmon-toga.cyclic.app/prediction")
 
 
 
